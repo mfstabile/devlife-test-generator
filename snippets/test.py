c381b092679ec458b30eb91266568ba9a02580cf
mfstabile <mf_stabile@hotmail.com>
2024-11-07 12:53:56 -0300

    commented max_points

--- snippets/test.py
+++ snippets/test.py
@@ -7,7 +7,7 @@
         for <parameters>, esperado in <tests>
     ]
 )
-@pytest.mark.max_points(0)
+# @pytest.mark.max_points(0)
 @pytest.mark.dependency_level(<dependency_level>)
 def test_<level>_etapa<phase>_<func_name>(<parameters>, esperado):
     assert_<func_name>_funciona(<parameters>, esperado)
@@ -23,7 +23,7 @@ def test_<level>_etapa<phase>_<func_name>(<parameters>, esperado):
         ]
     ]
 )
-@pytest.mark.max_points(0)
+# @pytest.mark.max_points(0)
 @pytest.mark.dependency_level(<dependency_level>)
 def test_<level>_etapa<phase>_<func_name>_exemplo1(<parameters>, esperado):
     assert_<func_name>_funciona(<parameters>, esperado, exemplo = " para o exemplo 1")
